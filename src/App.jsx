fb42d19e6788330f72205c24269c7c50f4451f13
RahJess <jessicakantotiana@gmail.com>
2025-07-29 21:30:35 +0200

    feat: import App and Home components in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './components/home'
 
 function App() {
-  
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Home />
+    </div>
+  );
 }
 
 export default App
