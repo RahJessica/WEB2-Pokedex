183d1b39984b65a9675f0fc2d2f01d3e9947bd26
nelio-gio <hei.rocques@gmail.com>
2025-07-29 21:13:01 +0300

    Affichage des cartes Pokémon: nom,image et numero

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -24,6 +24,20 @@ function Home() {
   }, []);
 
   if (loading) return <p>Chargement des Pokémons...</p>;
+   return (
+    <div className="p-4 grid grid-cols-2 md:grid-cols-4 gap-4">
+      {pokemonList.map((pokemon) => (
+        <div key={pokemon.id} className="bg-white rounded-xl shadow p-4 text-center">
+          <h2 className="font-bold capitalize text-lg">{pokemon.name}</h2>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} className="mx-auto w-20 h-20" />
+          <p>N° {pokemon.id}</p>
+        </div>
+      ))}
+    </div>
+  );
+
 }
+
+
 
 export default Home;
