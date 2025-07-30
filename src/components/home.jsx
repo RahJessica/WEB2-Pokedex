7bad52323028021459aee98238a2a098afe7194a
RahJess <jessicakantotiana@gmail.com>
2025-07-30 12:39:22 +0200

    chore: customization of background color of pokemon types

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,6 +4,27 @@ function Home() {
   const [pokemonList, setPokemonList] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  const typeColors = {
+    normal: "#A8A77A",
+    fire: "#EE8130",
+    water: "#6390F0",
+    electric: "#F7D02C",
+    grass: "#7AC74C",
+    ice: "#96D9D6",
+    fighting: "#C22E28",
+    poison: "#A33EA1",
+    ground: "#E2BF65",
+    flying: "#A98FF3",
+    psychic: "#F95587",
+    bug: "#A6B91A",
+    rock: "#B6A136",
+    ghost: "#735797",
+    dragon: "#6F35FC",
+    dark: "#705746",
+    steel: "#B7B7CE",
+    fairy: "#D685AD",
+  };
+
   useEffect(() => {
     async function loadPokemons() {
       const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20');
@@ -33,7 +54,13 @@ function Home() {
           <p>N° {pokemon.id}</p>
           <div className="mt-2">
             {pokemon.types.map((typeInfo) => (
-              <span key={typeInfo.slot} className="text-sm bg-gray-200 rounded px-2 py-1 mx-1">
+              <span
+                key={typeInfo.slot}
+                className="text-sm text-white font-semibold rounded px-2 py-1 mx-1"
+                style={{
+                  backgroundColor: typeColors[typeInfo.type.name] || '#777',
+                }}
+              >
                 {typeInfo.type.name}
               </span>
             ))}
