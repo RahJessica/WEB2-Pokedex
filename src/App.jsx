fffdc01753a95e3a4245fe8d0304f1c78f82d624
RahJess <jessicakantotiana@gmail.com>
2025-07-30 15:55:03 +0200

    add: implement searh-bar design component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './components/home'
+import SearchBar from './components/searchBar';
 
 function App() {
   return (
     <div className="min-h-screen">
+      <SearchBar />
       <Home />
-      <navbar />
     </div>
   );
 }
