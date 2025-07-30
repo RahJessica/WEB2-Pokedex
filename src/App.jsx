c6ee528cf77df942f7f24bb277ff13f438fa81e6
RahJess <jessicakantotiana@gmail.com>
2025-07-30 16:43:32 +0200

    fix: resolve merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './components/home'
+import NavBar from './components/navbar';
 import SearchBar from './components/searchBar';
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   return (
     <div className="min-h-screen">
+      <NavBar/>
       <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <Home searchTerm={searchTerm} />
     </div>
