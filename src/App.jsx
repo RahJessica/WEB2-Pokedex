09d3f922a5332e023dfccd06227af225f2390203
yola <miorayola07@gmail.com>
2025-07-30 17:28:45 +0200

    add: completed navbar with icons and colored borders

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './components/home'
+import NavBar from './components/navbar';
 
 function App() {
   return (
     <div className="min-h-screen">
+      <NavBar/>
       <Home />
-      <navbar />
     </div>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,30 @@
 import { Fragment, useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookOpen, faMedal, faPlus, faTrophy } from '@fortawesome/free-solid-svg-icons';
+import { faClipboard, faIdCard, faPlayCircle } from '@fortawesome/free-regular-svg-icons';
+
+const navItems=[
+    {name:"Accueil", icon: faMedal,  borderColor: "border-gray-500", hoverColor: 'hover:text-gray-500'},
+    {name: "Pokédex" , icon: faBookOpen, borderColor:"border-red-500", hoverColor: 'hover:text-red-500'},
+    {name: "Jeux vidéos et applis", icon : faPlus, borderColor: "border-orange-500", hoverColor: 'hover:text-orange-500'},
+    {name: "Jeu de cartes à collectionner", icon : faIdCard, borderColor: "border-yellow-500", hoverColor: 'hover:text-yellow-500'}, 
+    {name: "Dessins animés", icon: faPlayCircle, borderColor: "border-green-500",hoverColor: 'hover:text-green-500'},
+    {name:"Evènements play pokemon", icon : faTrophy, borderColor:"border-blue-300", hoverColor: 'hover:text-blue-300'},
+    {name:"Actualités", icon : faClipboard, borderColor:"border-blue-500", hoverColor: 'hover:text-blue-500'}
+]
 
 function NavBar (){
-    
+   return  <nav className="text-gray-500 flex py-4 px-20">
+        {navItems.map((item, index) => (
+        <button
+          key={index}
+         className={`flex flex-col items-center justify-around space-y-1 pb-2 border-b-4 ${item.borderColor}  ${item.hoverColor} transition flex-1`}
+        >
+          <FontAwesomeIcon icon={item.icon} className="text-xl" />
+          <span className="text-sm text-center">{item.name}</span>
+        </button>
+      ))};
+    </nav>
 }
+
+export default NavBar
